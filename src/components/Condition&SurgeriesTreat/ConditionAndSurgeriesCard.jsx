4f29c6f6c23fdc99c70b4bd9275666d239a28aca
Ajeet Singh <ajeetsinghjb@gmail.com>
2026-06-26 11:58:43 +0530

    again 2

--- src/components/Condition&SurgeriesTreat/ConditionAndSurgeriesCard.jsx
+++ src/components/Condition&SurgeriesTreat/ConditionAndSurgeriesCard.jsx
@@ -7,7 +7,7 @@ const ConditionAndSurgeriesCard = ({title,description}) => {
           <div className='d-flex flex-column px-2 text-start gap-1' style={{fontSize:'12px'}}>
              <div className='d-flex gap-2 '>
                 <span className='bi bi-check-circle' style={{color:'var(--website-primary)'}} ></span>
-                <div className='fw-semibold'>{title}</div>
+                <div className='fw-semibold custom-width'>{title}</div>
              </div>
             <div className="text-wrap ps-3 ms-1 custom-width">
                 {description}
